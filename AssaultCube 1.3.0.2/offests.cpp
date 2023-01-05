f47761ca194f5169d9297d28d8e3ed56b140591f
Sherlock <fhawaa@gmail.com>
2023-01-05 09:06:45 -0800

    Add files via upload

---
+++ AssaultCube 1.3.0.2/offests.cpp
@@ -0,0 +1,16 @@
+Game : AssaultCube 
+Version : 1.3.0.2
+localPlayer = [0017E0A8] [0018AC00]
+
+Health and armor                
+"ac_client.exe"+0017E0A8 [EC ] = health
+"ac_client.exe"+0017E0A8 [F0 ] = sheld
+Ammo                    
+"ac_client.exe"+0017E0A8 [140] = Assault Rifle
+"ac_client.exe"+0017E0A8 [12C] = Pistol
+"ac_client.exe"+0017E0A8 [148] = double pistol
+"ac_client.exe"+0017E0A8 [144] = bomb
+"ac_client.exe"+0018AC00 [13C] = Snipr
+ordnance 
+"ac_client.exe"+0017E0A8 [108] = pistol
+"ac_client.exe"+0017E0A8 [11C] = Assault Rifle
